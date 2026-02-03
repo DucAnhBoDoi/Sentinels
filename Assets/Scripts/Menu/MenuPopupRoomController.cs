5a2b0912fbecd2f7b9605b3d08b9041dd5fcccbb
Duc Anh <hoangducanh12a8@gmail.com>
2026-02-03 21:04:09 +0700

    Tab PopupRoom

--- Assets/Scripts/Menu/MenuPopupRoomController.cs
+++ Assets/Scripts/Menu/MenuPopupRoomController.cs
@@ -6,11 +6,82 @@ public class PopupRoomController : MonoBehaviour
     [SerializeField]
     private MenuSceneUtils _utils;
 
+    [Header("Buttons")]
     [SerializeField]
-    private Button _btnClose; 
+    private Button _btnClose;
+
+    [SerializeField]
+    private Button _btnTabOnline; 
+
+    [SerializeField]
+    private Button _btnTabLocal; 
+    
+    [SerializeField]
+    private Button _btnLoad;      
+
+    [Header("Content Containers")]
+    [SerializeField]
+    private GameObject _contentOnline; 
+
+    [SerializeField]
+    private GameObject _contentLocal;
+
+    private bool _isOnlineMode = true;
 
     private void Start()
     {
         _btnClose.onClick.AddListener(() => _utils.HidePopup());
+
+        _btnTabOnline.onClick.AddListener(OnBtnTabOnlineClick);
+        _btnTabLocal.onClick.AddListener(OnBtnTabLocalClick);
+
+        if (_btnLoad != null)
+        {
+            _btnLoad.onClick.AddListener(OnBtnLoadClick);
+        }
+
+        OnBtnTabOnlineClick();
+    }
+
+    private void OnBtnTabOnlineClick()
+    {
+        _isOnlineMode = true;
+
+        _contentOnline.SetActive(true);
+        _contentLocal.SetActive(false);
+
+        _btnTabOnline.interactable = false; 
+        _btnTabLocal.interactable = true;
+
+        Debug.Log("Đã chuyển sang Tab ONLINE");
+        // TODO: Sau này sẽ gọi hàm tìm phòng Online ở đây
+        // LobbyManager.Instance.RefreshList();
+    }
+
+    private void OnBtnTabLocalClick()
+    {
+        _isOnlineMode = false;
+
+        _contentOnline.SetActive(false);
+        _contentLocal.SetActive(true);
+
+        _btnTabOnline.interactable = true;
+        _btnTabLocal.interactable = false;
+
+        Debug.Log("Đã chuyển sang Tab LOCAL");
+        // TODO: Sau này sẽ gọi hàm tìm phòng LAN ở đây
+        // NetworkDiscovery.Search();
+    }
+
+    private void OnBtnLoadClick()
+    {
+        if (_isOnlineMode)
+        {
+            Debug.Log("Đang tải lại danh sách Online...");
+        }
+        else
+        {
+            Debug.Log("Đang quét lại mạng LAN...");
+        }
     }
 }
